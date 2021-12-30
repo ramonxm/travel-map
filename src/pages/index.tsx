2c6a7efcc89266bcc266b9b8b2b500fdf7d365b0
Ramon <ramon1234rxm@gmail.com>
2021-12-30 16:37:37 -0300

    chore: add husky and lint staged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+const bla = ''
+
 export default function Home() {
   return (
     <div className={styles.container}>
