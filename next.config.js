592d1d36f7bcff8f7c1c46d6b542db77b532e468
Ramon <ramon1234rxm@gmail.com>
2022-01-20 10:30:27 -0300

    chore: added pwa configs

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  }
+})
+
 module.exports = {
-  reactStrictMode: true,
+  reactStrictMode: true
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,8 @@ function App({ Component, pageProps }: AppProps) {
         <title>React Avançado</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#060092b" />
         <meta
           name="description"
           content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
